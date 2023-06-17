943673fdaa110b6b539298f85a845189e3e463c0
Maurice Ambroise <maurice.ambroise79@gmail.com>
2023-06-17 22:17:05 +0200

    add all2

--- src/main/java/com/example/activitea/controller/UserController.java
+++ src/main/java/com/example/activitea/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.activitea.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,12 @@ public class UserController {
 		} else {
 			return new ResponseEntity<String>(user.getName()+" "+user.getFirstname()+" n'a pu être créé",HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+	@GetMapping("/")
+	public String getmessage() {
+		return "Coucou tu gères";
+		
 	}
 	
 }
